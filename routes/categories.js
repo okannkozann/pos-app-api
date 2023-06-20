4e15c4dcfeaa351249b93de1089a51c01ade7aa5
okan kozan <okankzn11@hotmail.com>
2023-06-20 16:36:42 +0300

    .

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,7 @@ const Category = require("../models/Category.js");
 const express = require("express");
 const router = express.Router();
 
-//!get categories
+//! get all category
 router.get("/get-all", async (req, res) => {
   try {
     const categories = await Category.find();
@@ -12,33 +12,32 @@ router.get("/get-all", async (req, res) => {
   }
 });
 
-//! post category
+//! create
 router.post("/add-category", async (req, res) => {
   try {
     const newCategory = new Category(req.body);
     await newCategory.save();
-    res.status(200).json("Kategori basariyla eklendi.");
+    res.status(200).json("Item added successfully.");
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-//!put categories
+//! update
 router.put("/update-category", async (req, res) => {
   try {
     await Category.findOneAndUpdate({ _id: req.body.categoryId }, req.body);
-    res.status(200).json("Kategori basariyla guncellendi");
+    res.status(200).json("Item updated successfully.");
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-//!delete categories
-
+//! delete
 router.delete("/delete-category", async (req, res) => {
   try {
     await Category.findOneAndDelete({ _id: req.body.categoryId });
-    res.status(200).json("Kategori basariyla silindi");
+    res.status(200).json("Item deleted successfully.");
   } catch (error) {
     res.status(500).json(error);
   }
